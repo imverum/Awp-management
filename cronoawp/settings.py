17dea001c1ebbd5369dd32975695d22ef2269673
FESF <luizfilho@fesfsus.ba.gov.br>
2021-12-27 22:52:31 -0300

    Procfile

--- cronoawp/settings.py
+++ cronoawp/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'cronoawp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-default_dburl = 'sqlite:///' + os.path.join(BASE_DIR,'db.sqlite3')
+
 
 DATABASES = {
     'default': {
