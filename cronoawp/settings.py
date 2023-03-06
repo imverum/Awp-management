e177860c7412e2263b2d9afb2b276dcfa8ae98d7
Luiz Arthur <58349638+larthurfs@users.noreply.github.com>
2023-03-06 14:40:38 -0300

    djnago 3.2

--- cronoawp/settings.py
+++ cronoawp/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'cronoawp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mssql',
+        'ENGINE': 'mssql',
         'NAME': 'DB_ws5_homolog',
         'USER': 'app_ws5',
         'PASSWORD': '-e:faD^?^<yY2X8(',
